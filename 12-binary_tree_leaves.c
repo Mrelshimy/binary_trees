9a2853783b68b8796d91298916d576f13885b927
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-30 14:34:17 +0200

    fixing file 12 , updating valgrind .sh

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,14 +9,12 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves_count = 0;
+	size_t leaves_count = 1;
 
 	if (tree)
 	{
-		if (tree->right)
-			leaves_count++;
-		if (tree->left)
-			leaves_count++;
+		if (tree->right && tree->left)
+			leaves_count = 2;
 		return (leaves_count);
 	}
 
--- valgrind.sh
+++ valgrind.sh
@@ -1,5 +1,6 @@
 if [ "$1" ]; then
 	make leakcheck N="$1"
+	make clean
 else
 	echo "Enter the task number to check"
 fi
