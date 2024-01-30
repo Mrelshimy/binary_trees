eacec80a95438c53336912d383cbe11a31748f13
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-30 16:09:48 +0200

    dile 16 betty fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,17 +8,17 @@
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
-{	
+{
 	if (tree)
 	{
-		if (binary_tree_balance(tree) == 0)
-			{
-				if (binary_tree_leaves(tree->left) ==
-				binary_tree_leaves(tree->right))
-					return (1);
-			}
+		int balance = binary_tree_balance(tree);
+		size_t left_leaves = binary_tree_leaves(tree->left);
+		size_t right_leaves = binary_tree_leaves(tree->right);
+
+		if (balance == 0 && left_leaves == right_leaves)
+			return (1);
 		return (0);
-	}	
+	}
 	return (0);
 }
 
