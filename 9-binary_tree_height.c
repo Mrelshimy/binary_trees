998b49e7e32d73c7e63cf372821d8c47cf518ddd
Yahia <yn7599@gmail.com>
2024-01-30 19:35:05 +0200

    fixed task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t height_l;
+	size_t height_r;
 
-	if (tree)
-	{
-		left_height = 1 + binary_tree_height(tree->left);
-		right_height = 1 + binary_tree_height(tree->right);
-		if (left_height > right_height)
-			return (left_height);
-		else
-			return (right_height);
-	}
-	return (-1);
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
 }
