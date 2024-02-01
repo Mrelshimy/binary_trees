88e3cc6e67053b6655089c980cded578eb00f997
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-01 22:51:36 +0200

    bugfix

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -124,3 +124,32 @@ bst_t *one_child_nodes(bst_t *node, bst_t *root)
 	}
 	return (NULL);
 }
+
+/**
+ * bst_search - search for a value in BST
+ *
+ * @tree: pointer to tree
+ * @value: value to search for
+ * Return: pointer to node containing the value
+ */
+
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	bst_t *search_pointer = NULL;
+
+	if (tree)
+	{
+		search_pointer = (bst_t *)tree;
+		while (search_pointer)
+		{
+			if (search_pointer->n == value)
+				return (search_pointer);
+			if (search_pointer->n < value)
+				search_pointer = search_pointer->right;
+			else
+				search_pointer = search_pointer->left;
+		}
+
+	}
+	return (NULL);
+}
