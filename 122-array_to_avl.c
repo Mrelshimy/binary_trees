7e9ab7b41bb9a0d66c857a15a686cac18955c75d
Yahia <yn7599@gmail.com>
2024-02-02 03:34:39 +0200

    betty 121

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - inserts node to BST
+ * array_to_avl - creates an AVL tree from an array
  *
- * @tree: pointer to root of the tree to traverse
- * @value: node value
- * Return: pointer to inserted node , NULL if failed
+ * @array: array of integers
+ * @size: the size of the array
+ * Return: pointer to the root of the created tree
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-    avl_t *root;
+	avl_t *root;
 	size_t i = 0;
 
-	while(size > i)
+	while (size > i)
 		avl_insert(&root, array[i++]);
-    return (root);
+	return (root);
 }
